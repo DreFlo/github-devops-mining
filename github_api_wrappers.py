a13e32757eb1bde731de0a03774adea9b36f9b55
André Flores <andrejesusferflores@gmail.com>
2023-10-02 14:42:29 +0100

    Big bang

---
+++ github_api_wrappers.py
@@ -0,0 +1,316 @@
+from datetime import datetime, timedelta
+import sys
+import time
+import dateutil.parser
+import requests
+import json
+import os
+
+from dotenv import load_dotenv
+
+from requests.structures import CaseInsensitiveDict
+
+# Load Github Token
+load_dotenv()
+
+TOKEN = os.getenv('GITHUB_TOKEN')
+
+REQUEST_COUNT = 0
+
+RATE_LIMIT_RESET_TIME = None
+
+def send_get_request_wait_for_rate_limit(**kwargs) -> requests.Response:
+    global REQUEST_COUNT, RATE_LIMIT_RESET_TIME
+    REQUEST_COUNT += 1
+
+    if RATE_LIMIT_RESET_TIME:
+        if datetime.now() < RATE_LIMIT_RESET_TIME:
+            print(f'Rate limit reached, waiting until reset at {RATE_LIMIT_RESET_TIME}')
+            time.sleep((RATE_LIMIT_RESET_TIME - datetime.now()).total_seconds())
+            RATE_LIMIT_RESET_TIME = None
+    
+    response = requests.get(**kwargs)
+    
+    if int(response.headers['X-RateLimit-Remaining']) == '0':
+        RATE_LIMIT_RESET_TIME = datetime.fromtimestamp(float(response.headers['X-RateLimit-Reset']))
+
+    return response
+
+def reset_request_count() -> None:
+    global REQUEST_COUNT
+    REQUEST_COUNT = 0
+
+# Get repo names for query string
+def get_repository_full_names(query : str = 'path:.github/workflows extension:yaml', per_page : int = 5) -> list:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    # Query Parameters
+    params = CaseInsensitiveDict()
+    params['q'] = query
+    params['per_page'] = per_page
+    params['page'] = 1
+
+    url = 'https://api.github.com/search/code'
+
+    response = send_get_request_wait_for_rate_limit(url= url, params=params, headers=headers)
+
+    items = response.json()['items']
+
+    return [item['repository']['full_name'] for item in items]
+
+# Get repo information
+def get_repo(full_name : str) -> json:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = f'https://api.github.com/repos/{full_name}'
+
+    response = send_get_request_wait_for_rate_limit(url=url, headers=headers)
+
+    return response.json()
+
+# Get commits on default branch
+def get_repo_commits(full_name : str, per_page : str = 100, max_pages : str = sys.maxsize, since : str = None, until : str = None) -> list:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = f'https://api.github.com/repos/{full_name}/commits'
+
+    # Query Parameters
+    params = CaseInsensitiveDict()
+    params['per_page'] = per_page
+    params['page'] = 1
+    if since:
+        params['since'] = since
+    if until:
+        params['until'] = until
+
+    commits = []
+
+    # Go through pages to get all commits
+    while params['page'] <= max_pages:
+        response = send_get_request_wait_for_rate_limit(url=url, params=params, headers=headers)
+        commits = commits + response.json()
+        params['page'] += 1
+        # If number of commits retrieved is lesser than page limit, the current page is the last one
+        if len(response.json()) < 100:
+            break
+
+    return commits
+
+# Downloads repo contents to a zip
+def download_repo_snapshot(full_name : str, ref : str, path : str = '') -> None:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github.raw'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = f'https://api.github.com/repos/{full_name}/zipball/{path}'
+
+    # Query Parameters
+    params = CaseInsensitiveDict()
+    params['ref'] = ref
+
+    response = send_get_request_wait_for_rate_limit(url=url, params=params, headers=headers, allow_redirects=True)
+
+    with open(f'{full_name.split("/")[1]}-{ref[:6]}.zip', 'wb') as repo_contents:
+        repo_contents.write(response.content)
+
+    return None
+
+def get_repo_tree(full_name : str, tree_sha : str, recursive : bool = True) -> list:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = f'https://api.github.com/repos/{full_name}/git/trees/{tree_sha}'
+
+    # Query Parameters
+    params = CaseInsensitiveDict()
+    if recursive:
+        params['recursive'] = 'true'
+
+    response = send_get_request_wait_for_rate_limit(url=url, params=params, headers=headers)
+
+    return response.json()['tree']
+
+def get_commit_comparison(full_name : str, commit_1_sha : str, commit_2_sha) -> json:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    #headers['Accept'] = 'application/vnd.github.diff' # If you want the response to come with diff
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = f'https://api.github.com/repos/{full_name}/compare/{commit_1_sha}...{commit_2_sha}'
+
+    response = send_get_request_wait_for_rate_limit(url=url, headers=headers)
+
+    return response.json()
+
+# Get timestamp from commit in datetime object
+def get_commit_timestamp(commit : dict) -> datetime:
+    return dateutil.parser.isoparse(commit['commit']['author']['date'])
+
+# Get commits separated by roughly timedelta
+def get_repo_snapshots(commits : list, commit_interval : timedelta) -> list:
+    commits.sort(key=get_commit_timestamp)
+
+    repo_snapshots = []
+
+    for i in range(len(commits)):
+        if i == 0 or i == len(commits) - 1:
+            repo_snapshots.append(commits[i])
+        elif get_commit_timestamp(commits[i]) - get_commit_timestamp(repo_snapshots[-1]) >= commit_interval:
+            repo_snapshots.append(commits[i])
+
+    return repo_snapshots
+
+def get_snapshot_commits_retrieve_all_commits(full_name : str, commit_interval : timedelta = timedelta(days=90)) -> list:
+    commits = get_repo_commits(full_name=full_name)
+    return get_repo_snapshots(commits=commits, commit_interval=commit_interval)
+
+def get_snapshot_commits_query_timedelta(full_name : str, commit_interval : timedelta = timedelta(days=90)) -> list:
+    result = []
+
+    repo_info = get_repo(full_name=full_name)
+
+    created_at = dateutil.parser.isoparse(repo_info['created_at'])
+    updated_at = dateutil.parser.isoparse(repo_info['updated_at'])
+
+    # Get first commit
+    commits = get_repo_commits(full_name=full_name, until=(created_at + timedelta(days=7)).isoformat())
+
+    if len(commits) == 0:
+        print(f'Could not retrive first commit for {full_name}')
+        return []
+    
+    commits.sort(key=get_commit_timestamp)
+
+    result.append(commits[0])
+
+    # Get commits in intervals
+    day_window = timedelta(days=7)
+
+    while get_commit_timestamp(result[-1]) + commit_interval < updated_at:
+        since = (get_commit_timestamp(result[-1]) + commit_interval - day_window).isoformat()
+        until = (get_commit_timestamp(result[-1]) + commit_interval + day_window).isoformat()
+
+        commits = get_repo_commits(full_name=full_name, since=since, until=until)
+
+        # If no commits are retrieved, increase the day window and try again
+        if len(commits) == 0:
+            print(f'Could not retrive commits for {full_name} from {since} to {until}, increasing day window')
+            day_window *= 2
+            continue
+
+        day_window = timedelta(days=3)
+
+        commits.sort(key=get_commit_timestamp)
+
+        result.append(commits[-1])
+
+    # Get last commit
+    commits = get_repo_commits(full_name=full_name, per_page=1, max_pages=1)
+
+    result.append(commits[0])
+
+    return result
+
+def get_repo_contents(full_name : str, path : str = '', sha : str = None) -> list:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    params = CaseInsensitiveDict()
+    if sha:
+        params['sha'] = sha
+
+
+    url = f'https://api.github.com/repos/{full_name}/contents/{path}'
+
+    response = send_get_request_wait_for_rate_limit(url=url, headers=headers, params=params)
+
+    return response.json()
+
+def get_repo_trees(full_name : str, commits : list) -> list:
+    return [get_repo_tree(full_name=full_name, tree_sha=commit['sha'], recursive=True) for commit in commits]
+
+def get_api_rate_limits() -> json:
+    # Standard Request Header
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = 'application/vnd.github+json'
+    headers['Authorization'] = f'Bearer {TOKEN}'
+
+    url = 'https://api.github.com/rate_limit'
+
+    response = send_get_request_wait_for_rate_limit(url=url, headers=headers)
+
+    return response.json()
+
+full_names = get_repository_full_names(per_page=1)
+
+# print('Retrieve all commits')
+
+# start = time.time()
+
+commits_retrieve_all = get_snapshot_commits_retrieve_all_commits(full_name=full_names[0])
+
+# end = time.time()
+
+# print(f'Request Count: {REQUEST_COUNT}')
+
+# print(f'No. of commits retrieved: {len(commits_retrieve_all)}')
+
+# print(f'Time taken to retrieve all {len(commits_retrieve_all)} commits: {end - start}')
+
+# reset_request_count()
+
+# repo_contents = get_repo_contents(full_name=full_names[0], path='*.md', sha=commits_retrieve_all[-1]['sha'])
+
+# with open('response.json', 'w') as response_file:
+#     response_file.write(json.dumps(repo_contents, indent=2))
+
+# print('Wait 1 minute')
+
+# time.sleep(60)
+
+# print('Query timedelta')
+
+# start = time.time()
+
+# commits_query_timedelta = get_snapshot_commits_query_timedelta(full_name=full_names[0])
+
+# end = time.time()
+
+# print(f'Request Count: {REQUEST_COUNT}')
+
+# print(f'No. of commits retrieved: {len(commits_query_timedelta)}')
+
+# print(f'Time taken to retrieve all {len(commits_query_timedelta)} commits: {end - start}')
+
+# repo = get_repo(full_names[0])
+
+#commits = get_repository_commits(full_names[0])
+
+# print(f'No. of commits retrieved: {len(commits)}')
+# print(f'No. of commits selected: {len(repo_snapshots)}')
+
+#download_repo_snapshot(full_name=full_names[0], ref=repo_snapshots[-1]['sha'])
+#get_repo_tree(full_name=full_names[0], tree_sha=repo_snapshots[-1]['sha'])
+# get_commit_comparison(full_name=full_names[0], commit_1_sha=repo_snapshots[-2]['sha'], commit_2_sha=repo_snapshots[-1]['sha'])
+
+
+# with open('response_retrieve_all.json', 'w') as response_file:
+#     response_file.write(json.dumps(commits_retrieve_all, indent=2))
+
+# with open('response_query_timedelta.json', 'w') as response_file:
+#     response_file.write(json.dumps(commits_query_timedelta, indent=2))
---
+++ repo_analysis.py
@@ -0,0 +1,137 @@
+import re
+import time
+import dateutil.parser
+
+from github_api_wrappers import *
+from datetime import datetime, timedelta
+from enum import Enum
+
+class DevOpsTechnologies(Enum):
+    GITHUB_ACTIONS = 0
+    JENKINS = 1
+    TRAVIS_CI = 2
+    CIRCLE_CI = 3
+    GRADLE = 4
+    RAKE = 5
+    RANCHER = 6
+    DOCKER = 7
+    PROGRESS_CHEFF = 8
+    PUPPET = 9
+    NAGIOS = 10
+    PROMETHEUS = 11    
+
+# TODO Improve all check funtions, they are probably not very accurate
+def check_github_actions(node) -> bool:
+    return re.search(r'^\.github\/workflows\/.+\.ya?ml', node['path'])
+
+def check_jenkins(node) -> bool:
+    return re.search(r'Jenkinsfile', node['path'])
+
+def check_travis_ci(node) -> bool:
+    return re.search(r'\.travis\.ya?ml', node['path'])
+
+def check_circle_ci(node) -> bool:
+    return re.search(r'^(circle.ya?ml|\.circleci\/config\.ya?ml)', node['path'])
+
+def check_gradle(node) -> bool:
+    return re.search(r'build.gradle', node['path'])
+
+def check_rake(node) -> bool:
+    return re.search(r'Rakefile', node['path'])
+
+def check_rancher(node) -> bool:
+    return re.search(r'Kube_config_rancher-cluster\.ya?ml', node['path'])
+
+def check_docker(node) -> bool:
+    return re.search(r'Dockerfile', node['path'])
+
+def check_progress_cheff(node) -> bool:
+    return re.search(r'Metadata\.rb', node['path'])
+
+def check_puppet(node) -> bool:
+    return re.search(r'Site\.pp', node['path'])
+
+def check_nagios(node) -> bool:
+    return re.search(r'Nagios\.cfg', node['path'])
+
+def check_prometheus(node) -> bool:
+    return re.search(r'Prometheus\.ya?ml', node['path'])
+
+def get_devops_technologies(tree : list) -> list:
+    technologies = set()
+    for node in tree:
+        if not DevOpsTechnologies.GITHUB_ACTIONS in technologies and check_github_actions(node):
+            technologies.add(DevOpsTechnologies.GITHUB_ACTIONS)
+        elif not DevOpsTechnologies.JENKINS in technologies and check_jenkins(node):
+            technologies.add(DevOpsTechnologies.JENKINS)
+        elif not DevOpsTechnologies.TRAVIS_CI in technologies and check_travis_ci(node):
+            technologies.add(DevOpsTechnologies.TRAVIS_CI)
+        elif not DevOpsTechnologies.CIRCLE_CI in technologies and check_circle_ci(node):
+            technologies.add(DevOpsTechnologies.CIRCLE_CI)
+        elif not DevOpsTechnologies.GRADLE in technologies and check_gradle(node):
+            technologies.add(DevOpsTechnologies.GRADLE)
+        elif not DevOpsTechnologies.RAKE in technologies and check_rake(node):
+            technologies.add(DevOpsTechnologies.RAKE)
+        elif not DevOpsTechnologies.RANCHER in technologies and check_rancher(node):
+            technologies.add(DevOpsTechnologies.RANCHER)
+        elif not DevOpsTechnologies.DOCKER in technologies and check_docker(node):
+            technologies.add(DevOpsTechnologies.DOCKER)
+        elif not DevOpsTechnologies.PROGRESS_CHEFF in technologies and check_progress_cheff(node):
+            technologies.add(DevOpsTechnologies.PROGRESS_CHEFF)
+        elif not DevOpsTechnologies.PUPPET in technologies and check_puppet(node):
+            technologies.add(DevOpsTechnologies.PUPPET)
+        elif not DevOpsTechnologies.NAGIOS in technologies and check_nagios(node):
+            technologies.add(DevOpsTechnologies.NAGIOS)
+        elif not DevOpsTechnologies.PROMETHEUS in technologies and check_prometheus(node):
+            technologies.add(DevOpsTechnologies.PROMETHEUS)
+
+    return [technology.name for technology in technologies]
+
+api_limits = get_api_rate_limits()
+
+print(f'API rate limits: {api_limits}')
+
+repo_full_name = get_repository_full_names(per_page=1)[0]
+
+start = time.time()
+
+repo_all_commits = get_repo_commits(repo_full_name)
+
+end = time.time()
+
+print(f'Time taken to retrieve all {len(repo_all_commits)} commits: {end - start}')
+
+start = time.time()
+
+repo_snapshot_commits = get_repo_snapshots(repo_all_commits, timedelta(days=90))
+
+end = time.time()
+
+print(f'Time taken to retrieve snapshot commits: {end - start}')
+
+start = time.time()
+
+repo_snapshot_trees = get_repo_trees(full_name=repo_full_name, commits=repo_snapshot_commits)
+
+end = time.time()
+
+print(f'Time taken to retrieve {len(repo_snapshot_trees)} snapshot trees: {end - start}')
+
+start = time.time()
+
+repo_snapshot_devops_technologies = [get_devops_technologies(tree) for tree in repo_snapshot_trees]
+
+end = time.time()
+
+print(f'Time taken to retrieve snapshot devops technologies: {end - start}')
+
+print(repo_snapshot_devops_technologies)
+
+with open(f'{"_".join(repo_full_name.split("/"))}_snapshot_commits.json', 'w') as output_file:
+    output_file.write(json.dumps(repo_snapshot_commits, indent=2))
+
+with open(f'{"_".join(repo_full_name.split("/"))}_trees.json', 'w') as output_file:
+    output_file.write(json.dumps(get_repo_trees(full_name=repo_full_name, commits=repo_snapshot_commits), indent=2))
+
+with open(f'{"_".join(repo_full_name.split("/"))}_devops_technologies.json', 'w') as output_file:
+    output_file.write(json.dumps(repo_snapshot_devops_technologies, indent=2))
