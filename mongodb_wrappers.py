4a5a1a538854e130febc7ec499dedef2d46d71c4
André Flores <andrejesusferflores@gmail.com>
2023-10-25 09:26:02 +0100

    Use 2 dbs

--- mongodb_wrappers.py
+++ mongodb_wrappers.py
@@ -6,11 +6,15 @@ from pymongo.server_api import ServerApi
 from pymongo.cursor import Cursor
 
 URI = os.getenv("MONGODB_URI")
+URI_2 = os.getenv("MONGODB_URI_2")
 
 class MongoDBWrapper:
     def __init__(self, uri=URI):
         self.client = MongoClient(uri, server_api=ServerApi('1'))
         self.db = self.client["Repositories"]
+
+        self.client_2 = MongoClient(URI_2, server_api=ServerApi('1'))
+        self.db_2 = self.client_2["Repositories"]
 
     def get_collection(self, collection_name):
         return self.db[collection_name]
@@ -22,10 +26,10 @@ class MongoDBWrapper:
         return self.db["random"].find(filter, projection)
     
     def get_repo_tool_histories(self, filter : dict = {}, projection : dict = None) -> Cursor:
-        return self.db["repo_tools_history"].find(filter, projection)
+        return list(self.db["repo_tools_history"].find(filter, projection)) + list(self.db_2["repo_tools_history"].find(filter, projection))
     
     def add_repo_tools(self, repo_tools : dict):
-        self.db["repo_tools_history"].insert_one(repo_tools)
+        self.db_2["repo_tools_history"].insert_one(repo_tools)
         # TODO Uncomment this line when we are ready to mark the repo as processed
         # self.db["random"].update_one({"full_name" : repo_tools["repo_full_name"]}, {"retrieved_repo_histories" : True})
 
@@ -35,14 +39,17 @@ class MongoDBWrapper:
     def count_repo_snapshots(self, repo_name : str):
         repo_history = self.db['repo_tools_history'].find_one({"repo_full_name": repo_name})
         if repo_history is None:
+            repo_history = self.db_2['repo_tools_history'].find_one({"repo_full_name": repo_name})
+        if repo_history is None:
             return 0
         return len(repo_history['snapshots'])
     
     def has_been_processed(self, repo_name : str):
-        return self.db['repo_tools_history'].find_one({"repo_full_name": repo_name}) is not None
+        return self.db['repo_tools_history'].find_one({"repo_full_name": repo_name}) is not None or self.db_2['repo_tools_history'].find_one({"repo_full_name": repo_name}) is not None
     
     def delete_repo_histories(self):
         self.db["repo_tools_history"].delete_many({})
+        self.db_2["repo_tools_history"].delete_many({})
 
     def get_random_processed_repositories(self,size):
         mycol = self.db["random"]
